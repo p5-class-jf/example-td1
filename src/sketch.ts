b8e31c83862102e71295a5161e59f280305f0f98
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-22 11:07:47 +0100

    Use gaussianRandom

--- src/sketch.ts
+++ src/sketch.ts
@@ -6,9 +6,13 @@ const gui = new dat.GUI()
 const params = {
     N: 4000,
     Random_Seed: 0,
+    Dispersion: 0.14,
+    Opacity: 20,
     Download_Image: () => save(),
 }
 gui.add(params, "N", 0, 5000, 1)
+gui.add(params, "Dispersion", 0, 0.5, 0.0001)
+gui.add(params, "Opacity", 0, 255, 1)
 gui.add(params, "Random_Seed", 0, 100, 1)
 gui.add(params, "Download_Image")
 
@@ -20,11 +24,11 @@ function draw() {
     randomSeed(params.Random_Seed)
     background('#00d0ff')
     noStroke()
-    fill(255, 255, 255, 100)
+    fill(255, 255, 255, params.Opacity)
     translate(width/2, height/2)
     for (let i = 0; i < params.N; i++) {
         const angle = random(TWO_PI)
-        const radius = random(width / 4)
+        const radius = randomGaussian(0, width * params.Dispersion)
         ellipse(radius * cos(angle), radius * sin(angle), 30)
     }
 }
